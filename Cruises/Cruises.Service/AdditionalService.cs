7e9235874ec82e05993807b820317f497a61342c
salihe <salihe.bodeva10@gmail.com>
2023-08-16 22:39:40 +0300

    working on services

--- Cruises/Cruises.Service/AdditionalService.cs
+++ Cruises/Cruises.Service/AdditionalService.cs
@@ -8,14 +8,46 @@
     using System.Text;
     public class AdditionalService
     {
-        private  AppDbContext context;
-        public  City GetCityByName(string name)
+        private AppDbContext context;
+        public City GetCityByName(string name)
         {
             using (context = new AppDbContext())
             {
                 City c = this.context.Cities.FirstOrDefault(x => x.Name == name);
                 return c;
             }
+        }
+        public string CreateReservation(int passengerId, int voyageId, DateTime reservationDate)
+        {
+            StringBuilder message = new StringBuilder();
+            bool isValid = true;
+            if (passengerId < 1)
+            {
+                message.AppendLine("Invalid passenger id");
+                isValid = false;
+            }
+            if (voyageId < 1)
+            {
+                message.AppendLine("Invalid voyage id");
+                isValid = false;
+            }
+
+            using (context = new AppDbContext())
+            {
+                if (isValid)
+                {
+                    Reservation r = new Reservation()
+                    { 
+                        PassengerId=passengerId,
+                        VoyageId= voyageId,
+                        ReservationDate= reservationDate
+                    };
+                    context.Reservations.Add(r);
+                    context.SaveChanges();
+                    message.AppendLine($"Passenger got on board with his reservation on {reservationDate:g}");
+                }
+            }
+            return message.ToString().TrimEnd();
         }
     }
 }
