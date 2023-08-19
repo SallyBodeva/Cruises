0ac8c4b766a349832a059618a241f7fcfecf3bc7
salihe <salihe.bodeva10@gmail.com>
2023-08-19 16:13:26 +0300

    Form app

--- Cruises/Cruises.FormApp/MainForm.cs
+++ Cruises/Cruises.FormApp/MainForm.cs
@@ -10,9 +10,9 @@ using System.Windows.Forms;
 
 namespace Cruises.FormApp
 {
-    public partial class Form1 : Form
+    public partial class MainForm : Form
     {
-        public Form1()
+        public MainForm()
         {
             InitializeComponent();
         }
