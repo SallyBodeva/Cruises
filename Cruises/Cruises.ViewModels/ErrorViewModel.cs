d1149c790a64970b2a0902d04daccb48ab473b29
salihe <salihe.bodeva10@gmail.com>
2023-08-29 11:54:33 +0300

    Web app deleted due to error

--- Cruises/Cruises.ViewModels/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Cruises.ViewModels
-{
-    public class ErrorViewModel
-    {
-        public string RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
